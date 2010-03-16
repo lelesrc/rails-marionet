a4a067c89f3956a3916e67d20811ac59f057ee45
D.A <domang@domangi-MacBookPro.local>
2010-03-16 12:20:29 +0100

    added render functionality to slot class

--- slot.rb
+++ slot.rb
@@ -1,10 +1,11 @@
 class Slot
   require 'open-uri'
   require 'hpricot'
-  attr_accessor :url, :html
+  attr_accessor :url, :html, :name, :header, :body
   
-  def initialize(url)
+  def initialize(url,name)
     @url = url
+    @name = name
     begin
       f = open(url)
     rescue
@@ -12,17 +13,14 @@ class Slot
     else
       f.rewind
       @html = f.readlines.join("\n")
+      doc = Hpricot(@html)
+      @header = doc.search("head").to_s
+      @body = doc.search("body").to_s
       return true
     end
   end
   
-  def header
-    doc = Hpricot(self.html)
-    return doc.search("head").to_s
-  end
-  
-  def body
-    doc = Hpricot(self.html)
-    return doc.search("body").to_s
+  def render
+    return "<div class=\"#{self.name}\">#{self.body}</div>"
   end
 end
