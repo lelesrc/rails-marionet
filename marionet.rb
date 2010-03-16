27f4b88acd7cee62599ee69b0547fe0a61ac2f91
D.A <domang@domangi-MacBookPro.local>
2010-03-16 12:19:36 +0100

    added render functionality to marionet class

--- marionet.rb
+++ marionet.rb
@@ -3,30 +3,40 @@ class Marionet
   require 'slot'
   require 'logger'
   
-  @log = Logger.new('log/log.txt')
+  attr_accessor :slots
   
-  @PORTLETS ||= YAML.load_file('config/portlets.yml')
   #SET OF SLOT OBJECTS
-  @WIRES = []
   
-  def self.init
+  def initialize
+    @log = Logger.new('log/log.txt')
+    @PORTLETS ||= YAML.load_file('config/portlets.yml')
+    @slots = []   
+    
     @log.debug "initializing Marionet..."
     if not @PORTLETS.nil? then
       #LOAD ALL CONFIGURED PORTLETS
       @PORTLETS.each do |p|
         url = @PORTLETS[p[0]]['url']
-        if slot = Slot.new(url) then
-          @WIRES.push(slot)
+        name = @PORTLETS[p[0]]['name']
+        if slot = Slot.new(url,name) then
+          @slots.push(slot)
           @log.debug "New Slot added."
         else
           puts "ERROR CREATING SLOT #{url}"
           @log.debug "Errore creating slot."
         end
       end
-      return @WIRES
     else
       @log.debug "No Potlets configured."
-      return nil
     end
+  end
+  
+  def render
+    output = "<html><head></head><body>"
+    self.slots.each do |slot|
+      output = output + slot.render
+    end
+    output = output + "</body>"
+    return output
   end
 end
